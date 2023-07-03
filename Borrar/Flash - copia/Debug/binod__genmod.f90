a134a305c40388cc08c06b39b570437e92f92057
JPRovezzi <100390644+JPRovezzi@users.noreply.github.com>
2023-07-03 16:09:32 -0300

    Windows Support Added

--- Borrar/Flash - copia/Debug/binod__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Oct 28 17:10:29 2020
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE BINOD__genmod
-          INTERFACE 
-            SUBROUTINE BINOD
-            END SUBROUTINE BINOD
-          END INTERFACE 
-        END MODULE BINOD__genmod
