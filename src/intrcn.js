d6a051c9e69d22a72256caba3cadd4bd07e37d72
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-07-04 15:54:48 -0300

    parameters WiP

---
+++ src/intrcn.js
@@ -0,0 +1,62 @@
+//#region ram group category for for Liq./Liq.
+export var grouposram_table1 =[
+    ["None"],
+    ["(CH3)","(CH2)","(CH)","(C)"],
+    ["(CH2=CH)","(CH=CH)","(CH=C)","(CH2=C)"],
+    ["(ACH)","(AC)"],
+    ["(ACCH3)","(ACCH2)","(ACCH)"],
+    ["(OH)"],
+    ["(P1)"],
+    ["(P2)"],
+    ["(H2O)"],
+    ["(ACOH)"],
+    ["(CH3CO)","(CH2CO)"],
+    ["(CHO)"],
+    ["(FURF)"],
+    ["(COOH)","(HCOOH)"],
+    ["(CH3COO)","(CH2COO)"],
+    ["(CH3O)","(CH2O)","(CH-O)","(FCH2O)"],
+    ["(CH2CL)","(CHCL)","(CCL)"],
+    ["(CH2CL2)","(CHCL2)","(CCL2)"],
+    ["(CHCL3)","(CCL3)"],
+    ["(CCL4)"],
+    ["(ACCL)"],
+    ["(CH3CN)","(CH2CN)"],
+    ["(ACNH2)"],
+    ["(CH3NO2)","(CH2NO2)","(CHNO2)"],
+    ["(ACNO2)"],
+    ["(CH2OH)2"],
+    ["(DEOH)"],
+    ["(C5H5N)","(C5H4N)","(C5H3N)"],
+    ["(TCLE)"],
+    ["(MFA)"],
+    ["(DMFA)"],
+    ["(TMS)"],
+    ["(ME2SO)"]
+];
+//#endregion
+
+//#region ram group category for for Vap./Liq.
+export var grouposram_table2 =[]
+
+//#endregion
+
+//#region ram group category for for Inf.Dil
+export var grouposram_table3 =[]
+
+//#endregion
+
+
+//#region intrcn table for Liq./Liq.
+export var intrcn_table1 = [];
+
+//#endregion
+//#region intrcn table for Vap./Liq.
+export var intrcn_table2 = [];
+
+//#endregion
+
+//#region intrcn table for Inf.Dil.
+export var intrcn_table3 = [];
+
+//#endregion
