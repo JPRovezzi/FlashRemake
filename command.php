fc44fc7d6e3c664bcdff6b09f991428de51e122e
JPRovezzi <100390644+JPRovezzi@users.noreply.github.com>
2023-07-18 12:38:28 -0300

    .

--- command.php
+++
@@ -1,7 +0,0 @@
-<?php
-// execute the command that creates a file named hello.txt with the content "Hello, world!"
-exec("echo Hello, world! > hello.txt");
-
-// return a response to the client
-echo "Command executed successfully";
-?>
